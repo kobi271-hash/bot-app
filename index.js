292a6f42d6d9c2a8c1c18d57eb70334586793905
kobi271-hash <kobi271@gmail.com>
2026-04-02 23:44:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,12 @@
-const express = require("express");
-const axios = require("axios");
-
-const app = express();
-app.use(express.json());
-
-const PORT = process.env.PORT || 8080;
-
-const USERNAME = process.env.TV_USER;
-const PASSWORD = process.env.TV_PASS;
-const ACCOUNT_ID = process.env.ACCOUNT_ID;
-
-async function login() {
-  const res = await axios.post("https://live.tradovateapi.com/v1/auth/accesstokenrequest", {
-    name: USERNAME,
-    password: PASSWORD,
-    appId: "app",
-    appVersion: "1.0",
-    deviceId: "device"
-  });
-
-  return res.data.accessToken;
-}
-
-async function placeOrder(action) {
-  const token = await login();
-
-  console.log("Connected to Tradovate LIVE");
-
-  await axios.post(
-    "https://live.tradovateapi.com/v1/order/placeorder",
-    {
-      accountId: ACCOUNT_ID,
-      symbol: "MNQ1!",
-      action: action,
-      orderQty: 1,
-      orderType: "Market"
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
-
-  console.log("Order sent:", action);
-}
-
-app.post("/", async (req, res) => {
-  console.log("Received:", req.body);
-
-  const action = req.body.action;
-
-  if (action === "BUY" || action === "SELL") {
-    await placeOrder(action);
+{
+  "name": "bot-app",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.18.2"
   }
-
-  res.send("OK");
-});
-
-app.get("/", (req, res) => {
-  res.send("Bot is running");
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+}
